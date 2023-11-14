639eab6a19adade36c7a8a8f91db4b97d5a2fff4
Ariel <sonyt605@gmail.com>
2023-11-15 00:14:11 +0800

    layout-靜態結構搭建

--- vwv/vuw3-vwv/src/views/Layout/index.vue
+++ vwv/vuw3-vwv/src/views/Layout/index.vue
@@ -1,5 +1,12 @@
-<template>
-    <div>我是首頁</div>
-        <!-- 二級路由出口 -->
-        <RouterView />
-</template>
+<script setup>
+  import LayoutNav from './components/LayoutNav.vue'
+  import LayoutHeader from './components/LayoutHeader.vue'
+  import LayoutFooter from './components/LayoutFooter.vue'
+  </script>
+  
+  <template>
+    <LayoutNav />
+    <LayoutHeader />
+    <RouterView />
+    <LayoutFooter />
+  </template>
