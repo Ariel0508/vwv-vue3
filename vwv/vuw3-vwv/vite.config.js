cc5a0c4570fe39706d275e377e8b3a825655e376
Ariel <sonyt605@gmail.com>
2023-11-14 16:24:47 +0800

    axios基礎封裝

--- vwv/vuw3-vwv/vite.config.js
+++ vwv/vuw3-vwv/vite.config.js
@@ -1,16 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// elementPlus按照需求導入
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      // 1.配置elementPlus採用sass樣式配色系統
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    }),
   ],
   resolve: {
+    // 實際的路徑轉換 @ => src
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2.自動導入定制化樣式文件進行樣式覆蓋
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
-})
+});
