d7b4e245c2eb241f457b5d15d687d9666998fc3a
Ariel <sonyt605@gmail.com>
2023-11-21 18:18:35 +0800

    頭部購物車-列表渲染及消息框渲染

--- vwv/vuw3-vwv/src/views/Layout/components/LayoutHeader.vue
+++ vwv/vuw3-vwv/src/views/Layout/components/LayoutHeader.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useCategoryStore } from '@/stores/category'
-
+import HeaderCart from './HeaderCart.vue' 
 // 使用pinia中的數據
 const CategoryStore = useCategoryStore()
 </script>
@@ -21,7 +21,7 @@ const CategoryStore = useCategoryStore()
         <input type="text" placeholder="搜一搜">
       </div>
       <!-- 头部购物车 -->
-      
+      <HeaderCart />
     </div>
   </header>
 </template>
