7020067d3f45eb092b19bbb1fe8e0ae4ea39a122
Ariel <sonyt605@gmail.com>
2023-11-26 20:54:37 +0800

    會員中心-個人中心信息渲染

--- vwv/vuw3-vwv/src/apis/user.js
+++ vwv/vuw3-vwv/src/apis/user.js
@@ -1,15 +1,25 @@
-import httpInstance from '@/utils/http'
+import httpInstance from "@/utils/http";
 
 // 封裝所有和用戶相關的函數
 // 登入
 // 通過解構賦值的方式將參數傳過來
-export const loginAPI = ({account, password}) =>{
-return httpInstance({
-    url:'/login',
-    method: 'POST',
-    data:{
-        account,
-        password
-    }
-})
-}
+export const loginAPI = ({ account, password }) => {
+  return httpInstance({
+    url: "/login",
+    method: "POST",
+    data: {
+      account,
+      password,
+    },
+  });
+};
+
+// 封裝猜你喜歡接口
+export const getLikeListAPI = ({ limit = 4 }) => {
+  return httpInstance({
+    url: "/goods/relevant",
+    params: {
+      limit,
+    },
+  });
+};
